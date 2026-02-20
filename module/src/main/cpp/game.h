8e14f3b8ea68109a03e5679281c974428b284742
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-02-20 19:13:26 +0600

    change string trim logic

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -7,35 +7,38 @@
 
 #include <cstring>
 #include <cstdio>
+#include "log.h"
 
-// Read package name from /data/local/tmp/il2cpp_package.txt
-// If file doesn't exist or is empty, use the default package name
-inline const char* GetGamePackageName() {
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+
+inline const char *GetGamePackageName() {
     static char package_name[256] = {0};
     static bool initialized = false;
 
     if (!initialized) {
-        FILE *fp = fopen("/data/local/tmp/il2cpp_package.txt", "r");
+        FILE *fp = fopen("/data/adb/il2cppdumper/target.txt", "r");
         if (fp) {
             if (fgets(package_name, sizeof(package_name), fp)) {
-                // Remove trailing newline
-                size_t len = strlen(package_name);
-                if (len > 0 && (package_name[len - 1] == '\n' || package_name[len - 1] == '\r')) {
-                    package_name[len - 1] = '\0';
-                }
-                if (len > 1 && package_name[len - 2] == '\r') {
-                    package_name[len - 2] = '\0';
-                }
-                if (strlen(package_name) > 0) {
-                    fclose(fp);
-                    initialized = true;
-                    return package_name;
+                char *start = package_name;
+
+                while (isspace(static_cast<unsigned char>(*start))) start++;
+
+                char *end = start + strlen(start);
+                while (end > start && isspace(static_cast<unsigned char>(*(end - 1)))) end--;
+                *end = '\0';
+
+                if (start > package_name) {
+                    memmove(package_name, start, end - start + 1);
                 }
             }
             fclose(fp);
         }
-        // Fallback to default
-        strcpy(package_name, "com.example.game");
+
+        if (package_name[0] == '\0') {
+            snprintf(package_name, sizeof(package_name), "com.example.game");
+        }
         initialized = true;
     }
     return package_name;
